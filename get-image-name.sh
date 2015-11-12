60a57d85ba3975ea87ed4c665d88df7689e41956
ilyakorzh <ilya.korzh@ardas.dp.ua>
2015-11-12 17:47:45 +0200

    fix EOL

--- get-image-name.sh
+++ get-image-name.sh
@@ -1,4 +1,4 @@
 #clean all local Docker images if build & create & push is success
-# $1 = $JOBNAME
+# $1 = $JOB_NAME
 DockerName=$(echo "$1" | rev | cut -c 7- | rev)
 docker images | grep "$DockerName" | awk '{system("docker rmi -f "$3)}'
