1f4663f1c7932460669409c5cca52cb306c4bf01
ilyakorzh <ilya.korzh@ardas.dp.ua>
2015-11-12 17:45:05 +0200

    fix

--- get-image-name.sh
+++ get-image-name.sh
@@ -1,3 +1,4 @@
 #clean all local Docker images if build & create & push is success
-DockerName=$(echo "$JOB_NAME" | rev | cut -c 7- | rev)
+# $1 = $JOBNAME
+DockerName=$(echo "$1" | rev | cut -c 7- | rev)
 docker images | grep "$DockerName" | awk '{system("docker rmi -f "$3)}'
